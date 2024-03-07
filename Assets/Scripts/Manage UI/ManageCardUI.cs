419197de1d2f0a10ad36c148800a9dab71c6db43
Serafim <x.s.v.0202@gmail.com>
2024-03-07 14:27:42 +0300

    Добавил функционал залога

---
+++ Assets/Scripts/Manage UI/ManageCardUI.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ManageCardUI : MonoBehaviour
+{
+
+    [SerializeField] Image colorField;
+    [SerializeField] GameObject[] buildings;
+    [SerializeField] GameObject mortgageImage;
+    [SerializeField] TMP_Text mortgageValueText;
+    [SerializeField] Button mortgageButton, unMortgageButton;
+
+    Player playerReference;
+    MonopolyNode nodeReference;
+    //Color setColor, int numberOFBuildings, bool isMortgage, int mortgageValue
+    public ManageCardUI(MonopolyNode node, Player owner)
+    {
+        nodeReference = node;
+        playerReference = owner;
+        //Set Color
+        if (node.propertyColorField != null)
+        {
+            colorField.color = node.propertyColorField.color;
+        }
+        else
+        {
+            colorField.color = Color.black;
+        }
+        //Show buildings
+        if (node.NumberOfHouses < 4)
+        {
+            for (int i = 0; i < node.NumberOfHouses; i++)
+            {
+                buildings[i].SetActive(true);
+            }
+        }
+        else
+        {
+            buildings[4].SetActive(true);
+        }
+        //Show mortgage image
+        mortgageImage.SetActive(node.IsMortgaged);
+        //Text Update
+        mortgageValueText.text = "Стоимость залога = М " + node.MortgageValue;
+        //Buttons
+        mortgageButton.interactable = !node.IsMortgaged;
+        unMortgageButton.interactable = node.IsMortgaged;
+    }
+
+    public void MortgageButton()
+    {
+        if (nodeReference.IsMortgaged)
+        {
+            //Error messagge or such
+            return;
+        }
+        playerReference.CollectMoney(nodeReference.MortgageProperty());
+        mortgageImage.SetActive(true);
+        mortgageButton.interactable = false;
+        unMortgageButton.interactable = true;
+    }
+
+    public void UnMortgageButton()
+    {
+        if (!nodeReference.IsMortgaged)
+        {
+            //Error messagge or such
+            return;
+        }
+        playerReference.PayMoney(nodeReference.MortgageValue);
+        nodeReference.UnMortgageProperty();
+        mortgageImage.SetActive(false);
+        mortgageButton.interactable = true;
+        unMortgageButton.interactable = false;
+    }
+}
